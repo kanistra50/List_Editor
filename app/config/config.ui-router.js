308b2216557bdba2f5d55ba33067a497029be184
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-03-21 12:37:03 +0200

    Create config.ui-router.js

---
+++ app/config/config.ui-router.js
@@ -0,0 +1,21 @@
+angular.module("test", [
+    'ui.router'
+])
+
+    .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('/list');
+
+        $stateProvider
+            .state('list', {
+                url: '/list',
+                templateUrl: 'templates/list.html'
+            })
+            .state('add', {
+                url: '/list/add',
+                templateUrl: 'templates/form.html'
+            })
+            .state('edit', {
+                url: '/list/edit/:{itemId}',
+                templateUrl: 'templates/form.html'
+            });
+    }]);
