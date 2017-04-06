f3a2db2a2b645e53f4b311dc7083fae32592c49e
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-04-06 07:09:09 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,18 +1,21 @@
-angular.module("test", ['ui.router'])
-  .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+angular.module("test", [
+    'ui.router'
+])
+
+    .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider  ) {
         $urlRouterProvider.otherwise('/list');
 
         $stateProvider
             .state('list', {
                 url: '/list',
-                templateUrl: 'templates/list.html'
+                template: '<div dir-list>Directive</div>'
             })
             .state('add', {
                 url: '/list/add',
-                templateUrl: 'templates/form.html'
+                template: '<div dir-form>Directive</div>'
             })
             .state('edit', {
                 url: '/list/edit/:{itemId}',
-                templateUrl: 'templates/form.html'
+                template: '<div dir-form>Directive</div>'
             });
     }]);
