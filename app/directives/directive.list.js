e76ad8128b7d946a02ce8b471ea41dbdd83fb066
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-04-06 07:11:39 +0300

    Update directive.list.js

--- app/directives/directive.list.js
+++ app/directives/directive.list.js
@@ -1,1 +1,13 @@
-
+angular.module("test")
+    .directive('dirList', function () {
+        return {
+            scope: true,
+            restrict: "A",
+            templateUrl:'templates/list.html',
+            link: function (scope, element, attrs)
+            {
+                // scope.items = attrs.dirList;
+                console.log("scope d1 - " + scope.items);
+            }
+        };
+    });
