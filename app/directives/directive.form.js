a1c0fc6885322a4b50a0b5c9bc8b44722bc0f3a8
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-04-06 07:12:24 +0300

    Update directive.form.js

--- app/directives/directive.form.js
+++ app/directives/directive.form.js
@@ -1,1 +1,13 @@
-
+angular.module("test")
+    .directive('dirForm', function () {
+        return {
+            scope: true,
+            restrict: "A",
+            templateUrl:'templates/form.html',
+            link: function (scope, element, attrs)
+            {
+                // scope.items = attrs.dirForm;
+                console.log("scope d2 - " + scope.items);
+            }
+        }
+    });
